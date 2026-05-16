36c32c0d8a0171c0f8ed9c2668e69ccc7d78e05b
Levi Sousa <122644458+Levii-Sousa@users.noreply.github.com>
2026-05-16 12:34:52 -0300

    Use environment variable for server port

--- server.js
+++ server.js
@@ -63,4 +63,5 @@ app.delete('/clientes/:id', async (req, res) => {
 });
 
 // Inicialização do servidor
-app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
